25d85fb03261f968d3f0c158669622d16fecc81b
Michele <morticcino@diegoli.org>
2019-04-11 13:58:40 +0200

    Update explosion.js

--- js/explosion.js
+++ js/explosion.js
@@ -17,8 +17,8 @@ class explosion extends entity{
 			if(!me.frames) me.frames = me.__proto__.images[me.level]['EX'].width/gC.spriteW;
 			if(me.frame===me.frames){
 				assets[me.id].end = true;
-				if(me.type == 'demon') gC.Lifes--;
-				else gC.demonsCountdown--;
+				if(me.type == 'demon') gC.demonsCountdown--;
+				else gC.Lifes--;
 				res();
 			}else{
 				Utils.drawAnimation(me.__proto__.images[me.level]['EX'], gC.spriteW*me.frame, 0,gC.spriteW,gC.spriteH, me.randomX, me.randomY,gC.spriteW,gC.spriteH);
