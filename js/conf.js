187d43eed6ec8c7a6171f5a1692938af03d3a864
Michele <morticcino@diegoli.org>
2019-04-18 09:43:23 +0200

    Update conf.js

--- js/conf.js
+++ js/conf.js
@@ -10,7 +10,118 @@ var gC = {
     fr: 500,
     offset_arrow:5,
     offset_bullet:5,
-    score:0
+    score:0,
+    readDemonData: function(){
+        return new Promise(function(res,rej){
+            if(!gC.demonData){
+                var xmlhttp = new XMLHttpRequest();
+                var url = 'assets/games/demons/demons4js.json';
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        gC.demonData = JSON.parse(this.responseText);
+                        return res();
+                    }
+                };
+                xmlhttp.open("GET", url, true);
+                xmlhttp.send();
+            }else{
+                return res();
+            }
+        })
+
+
+    },
+    readShipData: function(){
+        return new Promise(function(res,rej){
+            if(!gC.shipData){
+                var xmlhttp = new XMLHttpRequest();
+                var url = 'assets/games/demonship/demonship/demonship4js.json';
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        gC.shipData = JSON.parse(this.responseText);
+                        return res();
+                    }
+                };
+                xmlhttp.open("GET", url, true);
+                xmlhttp.send();
+            }else{
+                return res();
+            }
+        })
+
+
+    },
+
+    readBackData: function(){
+        return new Promise(function(res,rej){
+            if(!gC.demonBack){
+                var xmlhttp = new XMLHttpRequest();
+                var url = 'assets/games/demonback/demonback4js.json';
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        gC.demonBack = JSON.parse(this.responseText);
+                        return res();
+                    }
+                };
+                xmlhttp.open("GET", url, true);
+                xmlhttp.send();
+            }else{
+                return res();
+            }
+        })
+
+
+    },
+
+    readControllerData: function(){
+        return new Promise(function(res,rej){
+            if(!gC.shipData){
+                var xmlhttp = new XMLHttpRequest();
+                var url = 'assets/games/controller.json';
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        gC.controller = JSON.parse(this.responseText);
+                        return res();
+                    }
+                };
+                xmlhttp.open("GET", url, true);
+                xmlhttp.send();
+            }else{
+                return res();
+            }
+        })
+
+
+    },
+    readAmbientData: function(){
+        return new Promise(function(res,rej){
+            if(!gC.ambient){
+                var xmlhttp = new XMLHttpRequest();
+                var url = 'assets/games/audio/ambient.json';
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        gC.ambient = JSON.parse(this.responseText);
+                        return res();
+                    }
+                };
+                xmlhttp.open("GET", url, true);
+                xmlhttp.send();
+            }else{
+                return res();
+            }
+        })
+
+
+    },
+    loadJsons: function(){
+        return new Promise((res,rej){
+                           
+        })
+    },
+    loadAmbientAudio: function(){
+        let i = Utils.random(1, Object.keys(gC.demonData.HE).length ).toString().padStart(2,'0');
+        gC.coin = new Audio('assets/games/audio/Coin001.wav');
+    }
 };
 
 gC.spritePosX = gC.width-gC.spriteW;
