7f4bde66e9ad94d29f1f922e50b3b4f85f032b5f
Michele <morticcino@diegoli.org>
2019-04-19 14:21:55 +0200

    Update new_engine.js

--- js/new_engine.js
+++ js/new_engine.js
@@ -499,6 +499,7 @@ function s(){
                     readControllerData().then(
                         (succ)=>{    
                             gC.coin.play();
+			    gC.ambient.play();
                             l();
                         }
                     )
