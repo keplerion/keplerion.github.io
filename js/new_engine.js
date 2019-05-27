0e0c1afd441cf5f35609fe37068727ea033cd969
Michele <morticcino@diegoli.org>
2019-05-27 08:39:32 +0200

    Update new_engine.js

--- js/new_engine.js
+++ js/new_engine.js
@@ -396,7 +396,7 @@ function addCanvas(){
                         gC.player.rightDown();
                         break;
                     case 32:
-                        assets.push(gC.player.fire('b_a','u'));
+                        assets.push(gC.player.fire('b_1','u'));
                         
                         break;
                 }
